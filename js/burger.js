a11a11d7704c9f490fc64e38e3b8868c73bca954
Lotredebz <thomas.p.pfau@gmail.com>
2025-05-16 11:23:23 +0200

    burgerv0.1

--- js/burger.js
+++ js/burger.js
@@ -1,2 +1,9 @@
 const btn = document.querySelector(".burger")
-const affichage = document.querySelector(".burgerdiv")
+const affichage = document.querySelector("header")
+btn.addEventListener("click", function(){
+    affichage.classList.toggle("burgeranim")
+
+    console.log(affichage.classList);
+})
+
+    
