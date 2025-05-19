7d643cee65b19426eb5ddbda5cb78da331ab4326
Lotredebz <thomas.p.pfau@gmail.com>
2025-05-19 14:31:33 +0200

    overflow burger

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,9 @@
 const btn = document.querySelector(".burger")
+const body = document.querySelector("body")
 const affichage = document.querySelector("header")
 btn.addEventListener("click", function(){
     affichage.classList.toggle("burgeranim")
-
+    body.classList.toggle("no__scroll")
     console.log(affichage.classList);
 })
 
