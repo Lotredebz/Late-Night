25374dbc292dd331556f80d7cb501cbc1294ea4b
Lotredebz <thomas.p.pfau@gmail.com>
2025-05-27 16:05:16 +0200

    update mineur

--- js/biblio.js
+++ js/biblio.js
@@ -1,8 +1,8 @@
 
-// var CHANNEL_ID = "UCmy6d_VwSZe1eRlCpyMaoCQ";
+// let CHANNEL_ID = "UCmy6d_VwSZe1eRlCpyMaoCQ";
 
 // function fetchVideos(order, containerId) {
-//     var url = "https://www.googleapis.com/youtube/v3/search?" +
+//     let url = "https://www.googleapis.com/youtube/v3/search?" +
 //     "key=" + API_KEY +
 //     "&channelId=" + CHANNEL_ID +
 //     "&part=snippet" +
@@ -14,21 +14,21 @@
 //         return response.json();
 //     })
 //     .then(function (data) {
-//         var videos = data.items;
-//         var container = document.getElementById(containerId);
+//         let videos = data.items;
+//         let container = document.getElementById(containerId);
 //         container.innerHTML = "";
 
-//         var count = 0;
-//         for (var i = 0; i < videos.length && count < 5; i++) {
-//         var video = videos[i];
+//         let count = 0;
+//         for (let i = 0; i < videos.length && count < 5; i++) {
+//         let video = videos[i];
 //         if (video.id.kind !== "youtube#video") {
 //             continue;
 //         }
 
-//         var videoId = video.id.videoId;
-//         var title = video.snippet.title;
+//         let videoId = video.id.videoId;
+//         let title = video.snippet.title;
 
-//         var div = document.createElement("div");
+//         let div = document.createElement("div");
 //         div.className = "video-card";
 //         div.innerHTML =
 //             "<iframe src='https://www.youtube.com/embed/" + videoId + "' frameborder='0' allowfullscreen></iframe>" +
@@ -45,24 +45,25 @@
 // fetchVideos("date", "recent");
 // fetchVideos("viewCount", "popular");
 
-    const rssUrl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCmy6d_VwSZe1eRlCpyMaoCQ';
+    const rssUrl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCX6OQ3DkcsbYNE6H8uQQuVA'; // UCmy6d_VwSZe1eRlCpyMaoCQ
     const apiUrl = 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(rssUrl);
 
     fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        const videoList = document.getElementById('video-list');
-        data.items.slice(0, 5).forEach(item => {
-          const videoId = item.link.split('=')[1];
-          const div = document.createElement('div');
-          div.className = 'video-card';
-          div.innerHTML = `
-            <iframe class="iframe" src="https://www.youtube.com/embed/${videoId}" allowfullscreen></iframe>
-            <div class="title">${item.title}</div>
-          `;
-          videoList.appendChild(div);
-        });
-      })
-      .catch(error => {
-        console.error('Erreur lors de la récupération des vidéos :', error);
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let videoList = document.getElementById('video-list');
+      data.items.slice(0, 5).forEach(function(item) {
+        let videoId = item.link.split('=')[1];
+        let div = document.createElement('div');
+        div.className = 'video-card';
+        div.innerHTML =
+          '<iframe class="iframe" src="https://www.youtube.com/embed/' + videoId + '" allowfullscreen></iframe>' +
+          '<div class="title">' + item.title + '</div>';
+        videoList.appendChild(div);
       });
+    })
+    .catch(function(error) {
+      console.error('Erreur lors de la récupération des vidéos :', error);
+  });
--- js/video.js
+++ js/video.js
@@ -2,18 +2,20 @@
 const channelID = 'UCmy6d_VwSZe1eRlCpyMaoCQ';
 const rssUrl = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelID}`;
 
-fetch(`https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(rssUrl)}`)
-  .then(res => res.json())
-  .then(data => {
-    const video = data.items[0];
+fetch('https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(rssUrl))
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    var video = data.items[0];
     console.log(data);
-    const videoID = video.link.split('=')[1];
-    const videoTitle = video.title;
+    var videoID = video.link.split('=')[1];
+    var videoTitle = video.title;
     document.getElementById('titreVideo').textContent = videoTitle;
-    const ajoutVid = `<iframe class="leblockvid" src="https://www.youtube.com/embed/${videoID}" allowfullscreen></iframe>`;
+    var ajoutVid = '<iframe class="leblockvid" src="https://www.youtube.com/embed/' + videoID + '" allowfullscreen></iframe>';
     document.getElementById("BlockVideo").innerHTML = ajoutVid;
   })
-  .catch(err => {
+  .catch(function(err) {
     console.error(err);
     document.getElementById('titreVideo').textContent = 'Impossible de charger le titre';
     document.getElementById("BlockVideo").innerHTML = '<p>Impossible de charger la video.</p>';
