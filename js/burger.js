68a931a1f6a9324ea9bf2801bcbfe2907eda3cf5
Lotredebz <thomas.p.pfau@gmail.com>
2025-06-05 10:48:19 +0200

    bug corriger

--- js/burger.js
+++ js/burger.js
@@ -1,10 +1,8 @@
 const btn = document.querySelector(".burger")
-const body = document.querySelector("body")
 const affichage = document.querySelector("header")
-btn.addEventListener("click", function(){
-    affichage.classList.toggle("burgeranim")
-    body.classList.toggle("no__scroll")
-    console.log(affichage.classList);
-})
 
-    
+btn.addEventListener("click", function(){
+    affichage.classList.toggle("burgeranim");
+    document.body.classList.toggle("no-scroll"); // Ajoute ou retire la classe
+    console.log(affichage.classList);
+});
